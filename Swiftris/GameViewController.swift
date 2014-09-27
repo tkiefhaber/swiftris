4e0b54dddd823f8990f76b5b071d1b1c1c6f2ea7
Tom Kiefhaber <tkiefhab@gmail.com>
2014-09-27 10:29:01 -0400

    background renders

--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
-
+    
+    var scene: GameScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let skView  = view as SKView
+        skView.multipleTouchEnabled = false
+        
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        skView.presentScene(scene)
 
     }
 
