f58afc27d2913ff34511b88e64f34058747fb6b0
Tom Kiefhaber <tkiefhab@gmail.com>
2014-09-27 21:59:02 -0400

    lots of work, still doesn't compile due to toRaw, fromRaw errors

---
+++ Swiftris/Shape.swift
@@ -0,0 +1,106 @@
+//
+//  Shape.swift
+//  Swiftris
+//
+//  Created by Thomas Kiefhaber on 9/27/14.
+//  Copyright (c) 2014 Thomas Kiefhaber. All rights reserved.
+//
+
+import SpriteKit
+
+let NumOrientations: UInt32 = 4
+
+enum Orientation: Int, Printable {
+    case Zero = 0, Ninety, OneEighty, TwoSeventy
+    
+    var description: String {
+        switch self {
+        case .Zero:
+            return "0"
+        case .Ninety:
+            return "90"
+        case .OneEighty:
+            return "180"
+        case .TwoSeventy:
+            return "270"
+            
+        }
+    }
+    
+    static func random() -> Orientation {
+        return Orientation.fromRaw(Int(arc4random_uniform(NumOrientations)))!
+    }
+    
+    static func rotate(orientation:Orientation, clockwise:Bool) -> Orientation {
+        var rotated = orientation.toRaw() + (clockwise ? 1 : -1)
+        if rotated > Orientation.TwoSeventy.toRaw() {
+            rotated = Orientation.Zero.toRaw()
+        } else if rotated < 0 {
+            rotated = Orientation.TwoSeventy.toRaw()
+        }
+        return orientation.fromRaw(rotated)!
+    }
+}
+
+let NumShapeTypes: UInt32 = 7
+let FirstBlockIdx: Int = 0
+let SecondBlockIdx: Int = 1
+let ThirdBlockIdx: Int = 2
+let FourthBlockIdx: Int = 3
+
+class Shape: Hashable, Printable {
+    let color:BlockColor
+    var blocks = Array<Block>()
+    var orientation: Orientation
+    var column, row:Int
+    
+    var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [:]
+    }
+    
+    var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [:]
+    }
+    
+    var bottomBlocks:Array<Block> {
+        if let bottomBlocks = bottomBlocksForOrientations[orientation] {
+            return bottomBlocks
+        }
+        return []
+    }
+    
+    var hashValue:Int {
+        return reduce(blocks, 0) { $0.hashValue ^ $1.hashValue }
+    }
+    
+    var description:String {
+        return "\(color) block facing \(orientation): \(blocks[FirstBlockIdx]), \(blocks[SecondBlockIdx]), \(blocks[ThirdBlockIdx]), \(blocks[FourthBlockIdx])"
+    }
+    
+    init(column:Int, row:Int, color: BlockColor, orientation:Orientation) {
+        self.color = color
+        self.column = column
+        self.row = row
+        self.orientation = orientation
+        initializeBlocks()
+    }
+    
+    convenience init(column:Int, row:Int) {
+        self.init(column:column, row:row, color:BlockColor.random(), orientation:Orientation.random())
+    }
+    
+    final func initializeBlocks() {
+        if let blockRowColumnTranslations = blockRowColumnPositions[orientation] {
+            for i in 0..<blockRowColumnTranslations.count {
+                let blockRow = row + blockRowColumnTranslations[i].rowDiff
+                let blockColumn = column + blockRowColumnTranslations[i].columnDiff
+                let newBlock = Block(column: blockColumn, row: blockRow, color: color)
+                blocks.append(newBlock)
+            }
+        }
+    }
+}
+
+func ==(lhs: Shape, rhs: Shape) -> Bool {
+    return lhs.row == rhs.row && lhs.column == rhs.column
+}
